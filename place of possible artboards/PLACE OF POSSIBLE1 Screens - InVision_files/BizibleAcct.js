40aaae60b6955647c4a8a95852b2ad74db23fd93
Carolyn <berrc603@newschool.edu>
2018-05-01 12:04:12 -0400

    final presentatoon

---
+++ place of possible artboards/PLACE OF POSSIBLE1 Screens - InVision_files/BizibleAcct.js
@@ -0,0 +1,12 @@
+(function () {
+    BizTrackingA.GoAccount({
+        webToLeadField: '',
+        chatEnabled: 'true' === 'true',
+        XUserId: '00f07e4ce7b145acd28963398ac1ef8c',
+        formProviderEnabled: 'true' === 'true',
+        attach_secure_forms: 'true' === 'true',
+        viewThroughEnabled: 'true' === 'true',
+        interceptAjax: 'true' === 'true'
+    });
+})();
+;
